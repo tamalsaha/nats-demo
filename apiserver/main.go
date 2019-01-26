00ec41ee0745dccb75bfd2de4a287f44edc58c62
Tamal Saha <tamal@appscode.com>
2019-01-26 04:26:52 -0800

    added-all

--- apiserver/main.go
+++ apiserver/main.go
@@ -4,10 +4,9 @@ import (
 	"io"
 	"log"
 
-	"github.com/nats-io/go-nats-streaming"
+	stan "github.com/nats-io/go-nats-streaming"
 )
 
-// Convenience function to log the error on a deferred close.
 func logCloser(c io.Closer) {
 	if err := c.Close(); err != nil {
 		log.Printf("close error: %s", err)
@@ -15,39 +14,29 @@ func logCloser(c io.Closer) {
 }
 
 func main() {
-	// Specify the cluster (of one node) and some client id for this connection.
-	conn, err := stan.Connect("test-cluster", "test-client")
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	conn, err := stan.Connect(
+		"test-cluster",
+		"test-client",
+		stan.NatsURL("nats://localhost:4222"),
+	)
 	if err != nil {
-		log.Print(err)
-		return
+		return err
 	}
 	defer logCloser(conn)
 
-	// Now the patterns..
-	handle := func(msg *stan.Msg) {
-		// If the msg is handled successfully, you can manually
-		// send an ACK to the server. More importantly, if processing
-		// fails, you can choose *not* send an ACK and you will receive
-		// the message again later.
-
-		// This will only fail if the connection with the server
-		// has gone awry.
-		if err := msg.Ack(); err != nil {
-			log.Printf("failed to ACK msg: %d", msg.Sequence)
+	// Publish up to 10.
+	for i := 0; i < 10; i++ {
+		err := conn.Publish("counter", nil)
+		if err != nil {
+			return err
 		}
 	}
 
-	sub, err := conn.Subscribe(
-		"stream-name",
-		handle,
-		stan.SetManualAckMode(),
-		stan.DurableName("i-will-remember"),
-	)
-	if err != nil {
-		log.Print(err)
-		return
-	}
-	defer logCloser(sub)
-
-	<- make(chan int)
+	return nil
 }
--- worker/main.go
+++ worker/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
+	"math/rand"
+	"sync/atomic"
+	"time"
 
-	"github.com/nats-io/go-nats-streaming"
+	stan "github.com/nats-io/go-nats-streaming"
 )
 
-// Convenience function to log the error on a deferred close.
 func logCloser(c io.Closer) {
 	if err := c.Close(); err != nil {
 		log.Printf("close error: %s", err)
@@ -15,39 +18,56 @@ func logCloser(c io.Closer) {
 }
 
 func main() {
-	// Specify the cluster (of one node) and some client id for this connection.
-	conn, err := stan.Connect("test-cluster", "test-client")
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+	<-make(chan interface{})
+}
+
+func run() error {
+	conn, err := stan.Connect(
+		"test-cluster",
+		"test-client",
+		stan.NatsURL("nats://localhost:4222"),
+	)
 	if err != nil {
-		log.Print(err)
-		return
+		return err
 	}
 	defer logCloser(conn)
 
-	// Now the patterns..
-	handle := func(msg *stan.Msg) {
-		// If the msg is handled successfully, you can manually
-		// send an ACK to the server. More importantly, if processing
-		// fails, you can choose *not* send an ACK and you will receive
-		// the message again later.
+	var lastProcessed uint64
+	var i int
 
-		// This will only fail if the connection with the server
-		// has gone awry.
-		if err := msg.Ack(); err != nil {
-			log.Printf("failed to ACK msg: %d", msg.Sequence)
+	sub, err := conn.Subscribe("counter", func(msg *stan.Msg) {
+		var processed bool
+
+		if msg.Sequence > lastProcessed {
+			processed = true
+			atomic.SwapUint64(&lastProcessed, msg.Sequence)
 		}
-	}
 
-	sub, err := conn.Subscribe(
-		"stream-name",
-		handle,
-		stan.SetManualAckMode(),
-		stan.DurableName("i-will-remember"),
-	)
+		// Add jitter..
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+
+		i++
+
+		var acked bool
+		if i <= 5 {
+			msg.Ack()
+			// Mark it is done.
+			acked = true
+		} else if i == 9 {
+			i = -5
+		}
+
+		// Print the value and whether it was redelivered.
+		fmt.Printf("seq = %d [redelivered = %v, acked = %v, processed = %v]\n", msg.Sequence, msg.Redelivered, acked, processed)
+
+	}, stan.SetManualAckMode(), stan.AckWait(time.Second))
 	if err != nil {
-		log.Print(err)
-		return
+		return err
 	}
 	defer logCloser(sub)
 
-	<- make(chan int)
+	return nil
 }
